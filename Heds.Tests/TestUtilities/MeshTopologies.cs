b4955883fa7fdbc0edcddef457db0023df7ab644
Andrew Moore <a.t.moore@gmail.com>
2021-11-07 07:25:32 -0800

    Update test failure message.

--- Heds.Tests/TestUtilities/MeshTopologies.cs
+++ Heds.Tests/TestUtilities/MeshTopologies.cs
@@ -9,7 +9,7 @@ namespace Heds.Tests.TestUtilities
         /// </summary>
         public static void AssertEquivalent(Mesh a, Mesh b)
         {
-            a.Should().NotBeSameAs(b, because: "this test will return false positives if a mesh is compared to itself.");
+            a.Should().NotBeSameAs(b, because: "testing a mesh against itself indicates a problem in the test");
             // TODO: needs a lot of work.
             a.Vertices.Should().HaveCount(b.Vertices.Count);
             a.Faces.Should().HaveCount(b.Faces.Count);
